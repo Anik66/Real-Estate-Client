81aeff08e3cb0f75658597be86eff48dd4779958
Anik66 <110911348+Anik66@users.noreply.github.com>
2024-04-16 19:04:15 -0600

    3rd commit

--- src/Pages/Deatils/Deatils.jsx
+++ src/Pages/Deatils/Deatils.jsx
@@ -1,10 +1,19 @@
+import { useParams } from "react-router-dom";
 
 
 const Deatils = () => {
+  const {estate_title,image,segment_name,price,id} =useParams
   return (
-    <div>
-      <h2>this is deatils page</h2>
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+    <figure><img src={image} alt="Album"/></figure>
+    <div className="card-body">
+      <h2 className="card-title">{estate_title}</h2>
+      <p>{segment_name}</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary">Listen</button>
+      </div>
     </div>
+  </div>
   );
 };
 
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -16,7 +16,7 @@ const router =createBrowserRouter([
       {
        path:"/",
        element:<Home></Home>,
-       loader:()=> fetch('FakeData.json')
+       loader:()=> fetch('/FakeData.json')
       },
       {
          path:'FakeData/:id',
