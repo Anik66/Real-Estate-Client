f046145043d0f9a8925589ef5624511599f81789
Anik66 <110911348+Anik66@users.noreply.github.com>
2024-04-18 10:19:26 -0600

    the last dance

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,12 +12,12 @@ const Navbar = () => {
    <li><NavLink to="blog">Blog</NavLink></li>
   
    
-   {
-    user && <li><NavLink to="update">Update Profile</NavLink></li>
-   }
-   {
-    user && <li><NavLink to="profile">Profile</NavLink></li>
-   }
+   <li>{
+    user && <NavLink to="update">Update Profile</NavLink>
+   }</li>
+   <li> {
+    user &&<NavLink to="profile">Profile</NavLink>
+   }</li>
    
   </div>
    </>
@@ -40,7 +40,7 @@ const Navbar = () => {
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
-        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <ul tabIndex={0} className="menu  dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
           {navlinks}
          
         </ul>
